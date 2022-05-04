36bc0a0036b148ed46c32b287f2fd420e7061c7f
Maria Victoria Casal <78768949+Vikycasal@users.noreply.github.com>
2022-05-04 19:24:27 -0300

    Update CardS.vue

--- vueapp/src/components/CardS.vue
+++ vueapp/src/components/CardS.vue
@@ -1,9 +1,10 @@
 <template>
    <div class="col-12 col-sm-4 col-md-3 col-lg-2 mb-3 mb-3">
        <div class="card">
-           <img :src="producto.image" alt="imagen-producto.image" class="card-img-top" />
            <div class="card-body">
+               <p class="card-text">{{producto.category}} <span></span></p>
                <h5 class="card-title">{{producto.title}}</h5>
+               <img :src="producto.image" alt="imagen-producto.image" class="card-img-top" />
                <p class="card-text">${{producto.price}} <span></span></p>
                <p class="card-text">{{producto.description}} <span></span></p>
                <button class="btn btn-dark" @click="agregar(producto)">Comprar</button>
